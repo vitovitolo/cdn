7e5b5441a771309d7dbc7b0c31c0d24fa025aaf9
vitovitolo <vitovito@gmail.com>
2017-05-30 15:50:30 +0200

    Added more serverspec tests: service running, installed and enabled. localhost ports listening

--- ansible/test/integration/default/serverspec/default_spec.rb
+++ ansible/test/integration/default/serverspec/default_spec.rb
@@ -2,12 +2,30 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe package('haproxy') do
-  it { should be_installed }
-end
+#Test every package are installed, enabled and runnning
+packages_list = [ "haproxy", "varnish", "nginx-extras" ]
 
-describe 'proxy' do
-  it 'responds on port 80' do
-    expect(port 80).to be_listening 'tcp'
+packages_list.each do |package|
+  describe package(package) do
+    it { should be_installed }
   end
 end
+
+describe service('haproxy') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+#Test localhost haproxy, nginx and varnish listen ports
+port_list = [ 8080, 8888, 8000, 6081, 6082 ]
+
+port_list.each do |port|
+  describe port(port) do
+    it { should be_listening.on('127.0.0.1').with('tcp') }
+  end
+end
+
+#describe file('/var/log/haprox_access.log') do
+#  it { should be_file }
+#  it { should be_readable.by('others') }
+#end
